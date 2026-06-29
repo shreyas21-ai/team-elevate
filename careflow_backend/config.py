dfa134721bb6736976c34857e8cd7f9e13fcdb65
shreyas21-ai <shreyu21shreyas@gmail.com>
2026-06-29 10:34:03 +0530

    renamed folder name

--- careflow_backend/config.py
+++ careflow_backend/config.py
@@ -2,5 +2,5 @@ import os
 
 HOST = os.getenv("DB_HOST", "localhost")
 USER = os.getenv("DB_USER", "root")
-PASSWORD = os.getenv("DB_PASSWORD", "")
+PASSWORD = os.getenv("DB_PASSWORD", "root")
 DATABASE = os.getenv("DB_NAME", "careflow")
